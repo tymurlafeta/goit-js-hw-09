2ef7fec8afe8a00ad16346c28938ffaa8c956ae2
tymurlafeta <126180100+tymurlafeta@users.noreply.github.com>
2023-06-05 14:09:49 +0000

    Deploying to gh-pages from @ tymurlafeta/goit-js-hw-09@79b799cdc0e302da3502f2e9c70af4e9f7a9de46 🚀

---
+++ 01-color-switcher.18b07b7d.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");function n(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}t.addEventListener("click",(function(){t.disabled=!0,intervalId=setInterval(n,1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(intervalId)}))}();
+//# sourceMappingURL=01-color-switcher.18b07b7d.js.map
