76b9db5b09b36597a8a95fc7766568aed1c3b00a
tymurlafeta <126180100+tymurlafeta@users.noreply.github.com>
2023-06-06 12:24:28 +0000

    Deploying to gh-pages from @ tymurlafeta/goit-js-hw-09@679a5f8827b6fc15c6b952c4fdd4264209205299 🚀

---
+++ 01-color-switcher.722ea7e6.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=null;function a(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}t.addEventListener("click",(function(){t.disabled=!0,n=setInterval(a,1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.722ea7e6.js.map
--- 01-color-switcher.a208245c.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");function n(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}t.addEventListener("click",(function(){t.disabled=!0,intervalId=setInterval(n,1e3)})),e.addEventListener("click",(function(){t.disabled=!1,clearInterval(intervalId)}));
-//# sourceMappingURL=01-color-switcher.a208245c.js.map
